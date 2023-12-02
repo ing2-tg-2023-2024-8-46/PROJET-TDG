2eab25aad7c30e6dc1373af7aaa8565852bb376c
lasbo <mathis.henry@edu.ece.fr>
2023-12-02 20:30:41 +0100

    update operations.h created operations.c

--- operations.h
+++ operations.h
@@ -5,10 +5,22 @@
 #ifndef PROJET_TDG_OPERATIONS_H
 #define PROJET_TDG_OPERATIONS_H
 
+#include <stdio.h>
+
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define GOLD "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define RESET_BOLD "\033[21m"
+#define RESET_COLOR "\033[39m"
+
+
 typedef struct {
 
     int max_operations;
-    int temps_cycle;
+    float temps_cycle_max;
 
 }t_infos;
 
@@ -30,10 +42,15 @@ typedef struct {
 
 typedef struct stravail {
 
+    float temps_cycle;
     int nb_operations;
     int* tab_operations;
     t_operation** tab_operations_struct;
 
 }t_station_travail;
+
+
+void afficher_station_travail(t_station_travail** tab_station_travail, int nb_station_travail, char* message);
+
 
 #endif //PROJET_TDG_OPERATIONS_H
