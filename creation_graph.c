4a289a47467c117e0f2c17f825df71394c6e761a
lasbo <mathis.henry@edu.ece.fr>
2023-12-02 19:47:12 +0100

    ptite maj

--- creation_graph.c
+++ creation_graph.c
@@ -18,11 +18,48 @@ void afficherGraphe(Graphe* g) {
     for (int i = 0; i < g->ordre; ++i) {
         if (g->tabSommet[i]->is_valid == 0) continue;
         printf("\n Sommet %d ", g->tabSommet[i]->valeur);
+        printf("\n\t - Degre %d ", g->tabSommet[i]->degre);
         printf("\n\t - Etape traitement %d ", g->tabSommet[i]->etape_traitement);
         printf("\n\t - Arcs : ");
         Arrete* arc = g->tabSommet[i]->arrete;
         while (arc != NULL) {
             printf("\n\t -> %d ", arc->sommet);
+            arc = arc->arrete_suivante;
+        }
+        printf("\n");
+        printf("\n");
+    }
+
+}
+
+void afficherGrapheColoration(Graphe* g) {
+
+
+    printf("Affichage du graphe\n");
+    printf("- Taille : %d\n", g->taille);
+    printf("- Ordre : %d\n", g->ordre);
+    printf("- Orientation : %d\n", g->orientation);
+    printf("- Sommets : \n");
+    for (int i = 0; i < g->ordre; ++i) {
+        if (g->tabSommet[i]->is_valid == 0) continue;
+        printf("\n Sommet %d ", g->tabSommet[i]->valeur);
+        if (g->tabSommet[i]->couleur == R) printf(RED" [R] "RESET);
+        if (g->tabSommet[i]->couleur == G) printf(GREEN" [G] "RESET);
+        if (g->tabSommet[i]->couleur == B) printf(BLUE" [B] "RESET);
+        if (g->tabSommet[i]->couleur == Y) printf(GOLD" [Y] "RESET);
+        if (g->tabSommet[i]->couleur == C) printf(CYAN" [C] "RESET);
+        if (g->tabSommet[i]->couleur == M) printf(MAGENTA" [M] "RESET);
+        printf("\n\t - Degre %d ", g->tabSommet[i]->degre);
+        printf("\n\t - Etape traitement %d ", g->tabSommet[i]->etape_traitement);
+        printf("\n\t - Arcs : ");
+        Arrete* arc = g->tabSommet[i]->arrete;
+        while (arc != NULL) {
+            if (g->tabSommet[arc->sommet]->couleur == R) printf(RED"\n\t -> %d "RESET, arc->sommet);
+            if (g->tabSommet[arc->sommet]->couleur == G) printf(GREEN"\n\t -> %d "RESET, arc->sommet);
+            if (g->tabSommet[arc->sommet]->couleur == B) printf(BLUE"\n\t -> %d "RESET, arc->sommet);
+            if (g->tabSommet[arc->sommet]->couleur == Y) printf(GOLD"\n\t -> %d "RESET, arc->sommet);
+            if (g->tabSommet[arc->sommet]->couleur == C) printf(CYAN"\n\t -> %d "RESET, arc->sommet);
+            if (g->tabSommet[arc->sommet]->couleur == M) printf(MAGENTA"\n\t -> %d "RESET, arc->sommet);
             arc = arc->arrete_suivante;
         }
         printf("\n");
--- creation_graph.h
+++ creation_graph.h
@@ -5,6 +5,24 @@
 #ifndef PROJET_TDG_CREATION_GRAPH_H
 #define PROJET_TDG_CREATION_GRAPH_H
 
+#define R 0
+#define G 1
+#define B 2
+#define Y 3
+#define C 4
+#define M 5
+
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define GOLD "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define RESET_BOLD "\033[21m"
+#define RESET_COLOR "\033[39m"
+
 
 typedef struct Arrete {
 
@@ -18,6 +36,8 @@ typedef struct Sommet {
 
     int is_valid;
     int valeur;
+    int degre;
+    int couleur;
     int etape_traitement;
     Arrete* arrete;
 
@@ -35,6 +55,7 @@ typedef struct Graphe{
 
 void CreerArete(Sommet** sommet, int s1, int s2, int poids);
 void afficherGraphe(Graphe* g);
+void afficherGrapheColoration(Graphe* g);
 
 
 
