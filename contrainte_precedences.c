be997a2d42e125b862700d36668efb8beb52da86
lasbo <mathis.henry@edu.ece.fr>
2023-12-02 18:04:04 +0100

    updated contrainte_precedences.c contrainte_precedences.h

--- contrainte_precedences.c
+++ contrainte_precedences.c
@@ -3,6 +3,7 @@
 //
 
 #include "contrainte_precedences.h"
+#include "creation_graph.h"
 #define INF (-1)
 
 // OBJECTIFS :
@@ -25,64 +26,6 @@
 //╚═╝░░░░░╚═╝░░╚═╝╚══════╝░╚════╝░╚══════╝╚═════╝░╚══════╝╚═╝░░╚══╝░╚════╝░╚══════╝╚═════╝░
 
 
-void afficherGraphe(Graphe* g) {
-
-
-    printf("Affichage du graphe\n");
-    printf("- Taille : %d\n", g->taille);
-    printf("- Ordre : %d\n", g->ordre);
-    printf("- Orientation : %d\n", g->orientation);
-    printf("- Sommets : \n");
-    for (int i = 0; i < g->ordre; ++i) {
-        if (g->tabSommet[i]->is_valid == 0) continue;
-        printf("\n Sommet %d ", g->tabSommet[i]->valeur);
-        printf("\n\t - Etape traitement %d ", g->tabSommet[i]->etape_traitement);
-        printf("\n\t - Arcs : ");
-        Arrete* arc = g->tabSommet[i]->arrete;
-        while (arc != NULL) {
-            printf("\n\t -> %d ", arc->sommet);
-            arc = arc->arrete_suivante;
-        }
-        printf("\n");
-        printf("\n");
-    }
-
-}
-
-
-void CreerArete(Sommet** sommet, int s1, int s2, int poids) {
-
-    if(sommet[s1]->arrete == NULL)
-    {
-        Arrete* Newarc=(Arrete*)malloc(sizeof(struct Arrete));
-        Newarc->sommet=s2;
-        Newarc->poids=poids;
-        Newarc->arrete_suivante=NULL;
-        sommet[s1]->arrete=Newarc;
-    }
-
-    else {
-        Arrete *temp = sommet[s1]->arrete;
-        while (temp->arrete_suivante != NULL) {
-            temp = temp->arrete_suivante;
-        }
-        Arrete *arc = (Arrete *) malloc(sizeof(struct Arrete));
-        arc->sommet = s2;
-        arc->poids = poids;
-        arc->arrete_suivante = NULL;
-
-        if (temp->sommet > s2) {
-            arc->arrete_suivante = temp->arrete_suivante;
-            arc->sommet = temp->sommet;
-            temp->sommet = s2;
-            temp->arrete_suivante = arc;
-        }
-
-        temp->arrete_suivante = arc;
-
-
-    }
-}
 
 Graphe* graph_precedences(t_infos* infos, t_operation** tab_operations) {
 
@@ -190,9 +133,8 @@ void contrainte_precedences(t_infos* infos, t_operation** tab_operations) {
         etape_traitement(g, i, 0, &max_etape);
     }
 
-    afficherGraphe(g);
 
-    t_station_travail** tab_station_travail;
+    t_station_travail** tab_station_travail = NULL;
     creation_station_travail(g, tab_station_travail, max_etape+1);
 
 
